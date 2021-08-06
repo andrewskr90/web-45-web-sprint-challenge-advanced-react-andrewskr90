f17cab7f060bf9310687a52088c80deb53fa176b
andrewskr90 <andrewskr90@gmail.com>
2021-08-06 12:06:09 -0700

    useForm custom hook working

--- src/components/CheckoutForm.js
+++ src/components/CheckoutForm.js
@@ -1,5 +1,5 @@
-import React, { useState } from "react";
-import { useForm } from "react-hook-form";
+import React from "react";
+import useForm from "../hooks/useForm";
 
 const initialValue = {
   firstName: "",
@@ -9,27 +9,14 @@ const initialValue = {
   state: "",
   zip: "",
 };
+const initialMessage = false
 
 // This form should be handled by a "useForm" custom hook
 // Build out the logic needed for a form custom hook (see the useForm.js file)
 // and replace the necessary stateful logic from CheckoutForm with the hook
 
 const CheckoutForm = (props) => {
-  const [values, handleSubmit, handleChanges, showSuccessMessage] = useForm(initialValue, false)
-
-  // const [showSuccessMessage, setShowSuccessMessage] = useState(false);
-
-
-  // const [values, setValues] = useState(initialValue);
-
-  // const handleChanges = (e) => {
-  //   setValues({ ...values, [e.target.name]: e.target.value });
-  // };
-
-  // const handleSubmit = (e) => {
-  //   e.preventDefault();
-  //   setShowSuccessMessage(true);
-  // };
+  const [values, handleSubmit, handleChanges, showSuccessMessage] = useForm(initialValue, initialMessage)  
 
   return (
     <>
--- src/hooks/useForm.js
+++ src/hooks/useForm.js
@@ -1,5 +1,20 @@
+import { useState } from 'react'
 // write your custom hook here to control your checkout form
 
 const useForm = (initValues, messageInitValue) => {
+    const [values, setValues] = useState(initValues)
+    const [showSuccessMessage, setShowSuccessMessage] = useState(messageInitValue);
+
+    const handleChanges = (e) => {
+      setValues({ ...values, [e.target.name]: e.target.value });
+    };
+
+    const handleSubmit = (e) => {
+      e.preventDefault();
+      setShowSuccessMessage(true);
+    };
     
+    return ([values, handleSubmit, handleChanges, showSuccessMessage])
 }
+
+export default useForm
