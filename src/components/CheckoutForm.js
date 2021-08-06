71a680ffc1d5cb556ccc08c61a538c5930d5e4d7
andrewskr90 <andrewskr90@gmail.com>
2021-08-06 11:36:12 -0700

    setup useForm custom hook parameters

--- src/components/CheckoutForm.js
+++ src/components/CheckoutForm.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { useForm } from "react-hook-form";
 
 const initialValue = {
   firstName: "",
@@ -14,17 +15,21 @@ const initialValue = {
 // and replace the necessary stateful logic from CheckoutForm with the hook
 
 const CheckoutForm = (props) => {
-  const [showSuccessMessage, setShowSuccessMessage] = useState(false);
-  const [values, setValues] = useState(initialValue);
+  const [values, handleSubmit, handleChanges, showSuccessMessage] = useForm(initialValue, false)
 
-  const handleChanges = (e) => {
-    setValues({ ...values, [e.target.name]: e.target.value });
-  };
+  // const [showSuccessMessage, setShowSuccessMessage] = useState(false);
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    setShowSuccessMessage(true);
-  };
+
+  // const [values, setValues] = useState(initialValue);
+
+  // const handleChanges = (e) => {
+  //   setValues({ ...values, [e.target.name]: e.target.value });
+  // };
+
+  // const handleSubmit = (e) => {
+  //   e.preventDefault();
+  //   setShowSuccessMessage(true);
+  // };
 
   return (
     <>
